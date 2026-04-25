181f28f6e83844823cd7311239e3954321c52abd
Abrar2003 <abrar.aalam003@gmail.com>
2026-04-25 17:51:43 +0530

    updated

--- client/OMS/src/utils/measuredSizeForm.js
+++ client/OMS/src/utils/measuredSizeForm.js
@@ -16,9 +16,9 @@ export const ITEM_SIZE_REMARK_OPTIONS = Object.freeze([
   { value: "item", label: "Item" },
   { value: "top", label: "Top" },
   { value: "base", label: "Base" },
+  { value: "item1", label: "Item 1" },
   { value: "item2", label: "Item 2" },
   { value: "item3", label: "Item 3" },
-  { value: "item4", label: "Item 4" },
 ]);
 
 export const BOX_SIZE_REMARK_OPTIONS = Object.freeze([
